b53afa7c81235a3b536c448406d12068fbac2ac8
shivpujan12 <shivpujanyadav1212@gmail.com>
2023-01-09 20:42:09 +0530

    Completed till insertion recur

--- src/Helper/Sorter.java
+++ src/Helper/Sorter.java
@@ -1,4 +1,5 @@
 package Helper;
+
 import java.util.*;
 
 /**
@@ -7,56 +8,55 @@ import java.util.*;
  * and quicksort in both technique iterative and recursive
  *
  * @author shivpujan
- *
  */
 public class Sorter {
     //bubble sort
-    int[] bubbleSortIter(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            for(int j=1;j<arr.length-i;j++){
-                if(arr[j-1]>arr[j]){
+    int[] bubbleSortIter(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j - 1] > arr[j]) {
                     //swap
-                     int temp = arr[j];
-                     arr[j] = arr[j-1];
-                     arr[j-1] = temp;
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                 }
             }
         }
         return arr;
     }
 
-    int[] bubbleSortR(int[] arr,int i,int j){
-        if(i==arr.length-1){
+    int[] bubbleSortR(int[] arr, int i, int j) {
+        if (i == arr.length - 1) {
             return arr;
         }
 
-        if(arr[j-1]>arr[j]){
+        if (arr[j - 1] > arr[j]) {
             //swap
             int temp = arr[j];
-            arr[j] = arr[j-1];
-            arr[j-1] = temp;
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
         }
-        if(j==arr.length-i-1){
+        if (j == arr.length - i - 1) {
             i++;
-            return bubbleSortR(arr,i,1);
+            return bubbleSortR(arr, i, 1);
         }
         j++;
-        return bubbleSortR(arr,i,j);
+        return bubbleSortR(arr, i, j);
 
     }
 
-    int[] bubbleSortRecur(int[] arr){
-        return bubbleSortR(arr,0,1);
+    int[] bubbleSortRecur(int[] arr) {
+        return bubbleSortR(arr, 0, 1);
     }
 
     //Selection Sort
-    int[] selectionSortIter(int[] arr){
-        for(int i=0;i<arr.length;i++){
+    int[] selectionSortIter(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
             int min = arr[i];
             int minInd = i;
-            for(int j=i+1; j<arr.length;j++){
+            for (int j = i + 1; j < arr.length; j++) {
                 //search for minimum
-                if(arr[j]<min){
+                if (arr[j] < min) {
                     minInd = j;
                     min = arr[j];
                 }
@@ -69,43 +69,44 @@ public class Sorter {
         return arr;
     }
 
-    int[] selectionSortR(int[] arr,int i,int j,int min,int minInd){
-        if(i==arr.length-1){
+    int[] selectionSortR(int[] arr, int i, int j, int min, int minInd) {
+        if (i == arr.length - 1) {
             return arr;
         }
 
         //search minimum
-        if(arr[j]<min){
+        if (arr[j] < min) {
             minInd = j;
             min = arr[j];
         }
 
-        if(j==arr.length-1){
+        if (j == arr.length - 1) {
             //found minimum make swap
             int temp = arr[i];
             arr[i] = arr[minInd];
             arr[minInd] = temp;
             i++;
-            j = i+1;
-            return selectionSortR(arr,i,j,arr[i],i);
+            j = i + 1;
+            return selectionSortR(arr, i, j, arr[i], i);
         } else {
             //iterate for searching minimum
             j++;
-            return selectionSortR(arr,i,j,min,minInd);
+            return selectionSortR(arr, i, j, min, minInd);
         }
 
     }
-    int[] selectionSortRecur(int[] arr){
-        return selectionSortR(arr,0,1,arr[0],0);
+
+    int[] selectionSortRecur(int[] arr) {
+        return selectionSortR(arr, 0, 1, arr[0], 0);
     }
 
     //Insertion Sort
-    int[] insertionSortIter(int[] arr){
-        for(int i=1; i<arr.length; i++){
+    int[] insertionSortIter(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
             int key = arr[i];
             int j = i;
-            while(j>0 && arr[j-1]>key){
-                arr[j] = arr[j-1];
+            while (j > 0 && arr[j - 1] > key) {
+                arr[j] = arr[j - 1];
                 j--;
             }
             arr[j] = key;
@@ -113,51 +114,48 @@ public class Sorter {
         return arr;
     }
 
-    int[] insertionSortR(int[] arr,int i,int j,int key){
+    int[] insertionSortR(int[] arr, int i, int j, int key) {
 
-        if(j>0 && arr[j-1]>key){//iterate j
-            arr[j] = arr[j-1];
+        if (j > 0 && arr[j - 1] > key) {//iterate j
+            arr[j] = arr[j - 1];
             j--;
-            return insertionSortR(arr,i,j,key);
+
         } else { //iterate i
             //make the swap with key
             arr[j] = key;
             i++;
-            if(i==arr.length){//terminate
+            if (i == arr.length) {//terminate
                 return arr;
             }
             key = arr[i];
             j = i;
-
-            return insertionSortR(arr,i,j,key);
-
         }
+        return insertionSortR(arr, i, j, key);
 
 
     }
 
-    int[] insertionSortRecur(int[] arr){
-        return insertionSortR(arr,1,1,arr[1]);
+    int[] insertionSortRecur(int[] arr) {
+        return insertionSortR(arr, 1, 1, arr[1]);
     }
-
 
 
     //unitTests
-    public void unitTest(){
+    public void unitTest() {
 //        testBubble();
 //        testSelection();
         testInsertion();
 
     }
 
-    public void testInsertion(){
+    public void testInsertion() {
         int[] arr = insertionSortIter(new int[]{1, 4, 2, 3});
         System.out.println("Insertion Sort(Iterative): " + Arrays.toString(arr));
         arr = insertionSortRecur(new int[]{1, 4, 2, 3});
         System.out.println("Insertion Sort(Recursive): " + Arrays.toString(arr));
     }
 
-    public void testSelection(){
+    public void testSelection() {
         int[] arr = selectionSortIter(new int[]{1, 4, 2, 3});
         System.out.println("Selection Sort(Iterative): " + Arrays.toString(arr));
         arr = selectionSortRecur(new int[]{1, 4, 2, 3});
@@ -165,13 +163,12 @@ public class Sorter {
     }
 
 
-    public void testBubble(){
+    public void testBubble() {
         int[] arr = bubbleSortIter(new int[]{1, 4, 2, 3});
         System.out.println("Bubble Sort(Iterative): " + Arrays.toString(arr));
         arr = bubbleSortRecur(new int[]{1, 4, 2, 3});
         System.out.println("Bubble Sort(Recursive): " + Arrays.toString(arr));
     }
-
 
 
 }
